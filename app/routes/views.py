a99621ce5b4c1bec5368c990324e293582489076
Tamir Suliman <allamiro@gmail.com>
2024-11-17 19:32:14 +0000

    updating the html files and fix the login page css issue

--- app/routes/views.py
+++ app/routes/views.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, url_for
+from flask_login import current_user
 
 views_bp = Blueprint("views", __name__)
 
 @views_bp.route("/")
 def home():
-    return render_template("home.html")
+    if not current_user.is_authenticated:
+        return redirect(url_for("auth.login"))
+    return redirect(url_for("compliance.dashboard"))  # Redirect authenticated users to the dashboard
